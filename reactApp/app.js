d381f0aba00e1a212eff43ad48225f62451e60fa
mhelmrich <33710207+mhelmrich@users.noreply.github.com>
2018-07-26 15:31:47 -0700

    Add todo list

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,5 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p>React lives!</p>,
+var dummyData = ["Learn about React", "Finish todo project", "Horizons speaker series"];
+
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <li>{this.props.task}</li>
+    )
+  }
+}
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <ul>
+        {dummyData.map((item) => (<Todo key={item} task={item}/>))}
+      </ul>
+    )
+  }
+}
+
+ReactDOM.render(<TodoList />,
    document.getElementById('root'));
