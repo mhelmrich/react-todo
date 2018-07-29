9f5295ca69124f754e0fa25577efa608465bf92c
mhelmrich <33710207+mhelmrich@users.noreply.github.com>
2018-07-28 17:04:33 -0700

    adding todos

--- reactApp/components/InputLine.js
+++ reactApp/components/InputLine.js
@@ -3,12 +3,23 @@ import React from 'react';
 class InputLine extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      text: ''
+    }
+  }
+  handleTyping(event) {
+    this.setState({text: event.target.value});
+  }
+  handleSubmit(event) {
+    this.props.submit(this.state.text);
+    this.setState({text: ''});
   }
   render() {
     return (
       <div>
-        <input type="text" placeholder="your task"/>
-        <button onClick={() => this.props.submit("Test Task")}>Submit</button>
+        <input type="text" value={this.state.text} placeholder="Add a task.."
+          onChange={(event) => this.handleTyping(event)}/>
+        <button onClick={(event) => this.handleSubmit(event)}>Submit</button>
       </div>
     )
   }
