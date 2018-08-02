fd2e65e7eee6c2ea6a9108cfb47684edd7061622
mhelmrich <33710207+mhelmrich@users.noreply.github.com>
2018-08-02 15:47:58 -0700

    hashing passwords

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const crypto = require('crypto');
 const router = express.Router();
 const User = require('../models/User.js');
+
+function hashPassword(password) {
+  let hash = crypto.createHash('sha256');
+  hash.update(password);
+  return hash.digest('hex');
+}
 
 module.exports = function(passport) {
 
@@ -10,6 +17,7 @@ module.exports = function(passport) {
   })
 
   router.post('/login', (req, res, next) => {
+    req.body.password = hashPassword(req.body.password);
     passport.authenticate('local', (err, user) => {
       if (err) return res.status(500).json({success: false});
       if (!user) return res.status(400).json({success: false});
@@ -24,7 +32,7 @@ module.exports = function(passport) {
     if (req.body.username && req.body.password) {
       (new User({
         username: req.body.username,
-        password: req.body.password,
+        password: hashPassword(req.body.password)
       }))
       .save().then(() => res.json({success: true}))
       .catch((err) => res.status(500).json({success: false}));
